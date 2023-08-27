500212ce2a7f369956adfa5047334c4ceed23385
Sudono Tanjung <sudtanj.business@gmail.com>
2023-08-27 09:53:49 +0700

    init project counter ios

---
+++ Counter/Counter/Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by Sudono Tanjung on 05/08/23.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
